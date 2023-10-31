98c4d659f19839153e98229daf5ca5eb4ad0e3fa
Sanket <sanketgupta1000@gmail.com>
2023-10-31 10:13:14 +0530

    BST algos

---
+++ Tree/BST/BST_Insert.cpp
@@ -0,0 +1,169 @@
+//Problem Link: https://practice.geeksforgeeks.org/problems/insert-a-node-in-a-bst/1?page=1&category=Binary%20Search%20Tree&sortBy=submissions
+
+//Solution:
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// Function to insert a node in a BST.
+
+/*
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+//iterative solution
+
+class Solution
+{
+public:
+    Node* insert(Node* root, int key)
+    {
+        //pointer to link to be changed
+        Node ** link_addr = &root;
+        
+        while(((*link_addr)!=nullptr)&&(((*link_addr)->data)!=key))
+        {
+            if(((*link_addr)->data)<key)
+            {
+                link_addr = &((*link_addr)->right);
+            }
+            else
+            {
+                link_addr = &((*link_addr)->left);
+            }
+        }
+        
+        if((*link_addr)==nullptr)
+        {
+            //same key not found, so can be inserted
+            (*link_addr) = new Node(key);
+        }
+        return root;
+        
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void inorder(Node* root, vector<int>& v) {
+    if (root == NULL) return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        getline(cin, s);
+        int k = stoi(s);
+        // getline(cin, s);
+        Solution ob;
+        ob.insert(root, k);
+        vector<int> v;
+        inorder(root, v);
+        for (int i = 0; i < v.size(); i++) cout << v[i] << " ";
+        cout << endl;
+
+        // cout<<"~"<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
